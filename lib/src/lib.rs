3bf406cc9fd0de0d1b5ec6c673d5b820cbe778d6
artemp <artmpktn@gmail.com>
2024-12-01 00:22:12 +0200

    feat: init base implementation of hash function

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,8 @@
+use serde::{Deserialize, Serialize};
 use uint::construct_uint;
-construct_uint!{
+
+construct_uint! {
+  #[derive(Serialize, Deserialize)]
   pub struct U256(4);
 }
 
--- lib/src/sha256.rs
+++ lib/src/sha256.rs
@@ -1,1 +1,21 @@
-pub struct Hash;
+use crate::U256;
+use serde::{Deserialize, Serialize};
+use sha256::digest;
+
+#[derive(Clone, Copy, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct Hash(U256);
+
+impl Hash {
+    pub fn hash<T: serde::Serialize>(data: &T) -> Self {
+        let mut serialized: Vec<u8> = vec![];
+        if let Err(e) = ciborium::into_writer(data, &mut serialized) {
+            panic!("Failed to serialize data {:?}", e);
+        }
+
+        let hash = digest(&serialized);
+        let hash_bytes = hex::decode(hash).unwrap();
+        let hash_array: [u8; 32] = hash_bytes.as_slice().try_into().unwrap();
+
+        Hash(U256::from(hash_array))
+    }
+}
