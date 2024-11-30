384298e40893362b35f6c422051cc931af5f2d52
artemp <artmpktn@gmail.com>
2024-11-30 22:54:15 +0200

    feat: init dummy impl of base structs

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -1,4 +1,97 @@
-pub struct Blockchain;
-pub struct Block;
-pub struct BlockHeader;
-pub struct Transaction;
+use crate::U256;
+use uuid::Uuid;
+
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+}
+
+pub struct Block {
+    pub header: BlockHeader,
+    pub transactions: Vec<Transaction>,
+}
+
+pub struct BlockHeader {
+    /// Timestamp of the block
+    pub timestamp: u64,
+    /// Nonce used to mine the block
+    pub nonce: u64,
+    /// Hash of the previous block
+    pub prev_block_hash: [u8; 32],
+    /// Merkle root of the block's transactions
+    pub merkle_root: [u8; 32],
+    /// target
+    pub target: U256,
+}
+
+pub struct Transaction {
+    pub inputs: Vec<TransactionInput>,
+    pub outputs: Vec<TransactionOutput>,
+}
+
+/// txin
+pub struct TransactionInput {
+    pub prev_transaction_output_hash: [u8; 32],
+    pub signature: [u8; 64],
+}
+
+/// txout
+pub struct TransactionOutput {
+    pub value: u64,
+    pub unique_id: Uuid,
+    pub pubkey: [u8; 33],
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Blockchain { blocks: vec![] }
+    }
+
+    pub fn add_block(&mut self, block: Block) {
+        self.blocks.push(block)
+    }
+}
+
+impl Block {
+    pub fn new(header: BlockHeader, transactions: Vec<Transaction>) -> Self {
+        Block {
+            header,
+            transactions,
+        }
+    }
+
+    pub fn hash(&self) -> ! {
+        todo!()
+    }
+}
+
+impl BlockHeader {
+    pub fn new(
+        timestamp: u64,
+        nonce: u64,
+        prev_block_hash: [u8; 32],
+        merkle_root: [u8; 32],
+        target: U256,
+    ) -> BlockHeader {
+        BlockHeader {
+            timestamp,
+            nonce,
+            prev_block_hash,
+            merkle_root,
+            target,
+        }
+    }
+
+    pub fn hash() -> ! {
+        todo!()
+    }
+}
+
+impl Transaction {
+    pub fn new(inputs: Vec<TransactionInput>, outputs: Vec<TransactionOutput>) -> Transaction {
+        Transaction { inputs, outputs }
+    }
+
+    pub fn hash() -> ! {
+        todo!()
+    }
+}
