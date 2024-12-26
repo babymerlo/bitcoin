0aa64b0a2cda97becb836bd5680c4937405070f4
Artem <artmpktn@gmail.com>
2024-12-26 02:16:09 +0200

    feat: add lib/bin, generate some examples

---
+++ lib/src/bin/tx_print.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::process::exit;
+
+use btclib::types::Transaction;
+use btclib::util::Saveable;
+
+fn main() {
+    let path = if let Some(arg) = env::args().nth(1) {
+        arg
+    } else {
+        eprintln!("Usage: tx_print <tx_file>");
+        exit(1);
+    };
+
+    if let Ok(file) = File::open(path) {
+        let tx = Transaction::load(file).expect("Failed to load transaction");
+        println!("{:#?}", tx);
+    }
+}
